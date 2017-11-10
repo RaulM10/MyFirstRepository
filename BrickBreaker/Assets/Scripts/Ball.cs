9b62e152fdc912c1f0948cd2b0097202f3e4e5da
Raul Mifsud <b42079@mcast.org.mt>
2017-11-10 14:23:18 +0100

    Set Paddle to move with mouse position (in game units) Locked the ball to move with the paddle at the start of the game Upon first left button click, the ball will begin to bounce

---
+++ BrickBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+    Vector3 PaddleBallPosDiff;
+    bool GameStarted = false;
+	// Use this for initialization
+	void Start () {
+
+        PaddleBallPosDiff = this.transform.position - paddle.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!GameStarted)
+        {
+            this.transform.position = paddle.transform.position + PaddleBallPosDiff;
+        }
+
+        if(Input.GetMouseButtonDown(0) && !GameStarted)
+        {
+            GameStarted = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+        }
+
+
+
+    }
+}
---
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        float MousePosUnits = (Input.mousePosition.x / Screen.width * 16) - 8;
+
+        Vector3 NewPaddlePos = new Vector3(MousePosUnits, this.transform.position.y, this.transform.position.z);
+
+        NewPaddlePos.x = Mathf.Clamp(MousePosUnits, -7.36f, 7.36f);
+
+        this.transform.position = NewPaddlePos;
+
+    }
+}
