42597cdee484e2cb26774507ca514b9ad205a4a5
Raul Mifsud <b42079@mcast.org.mt>
2017-12-07 11:15:15 +0100

    - Made both of the Paddles of the Players to have a RigidBody 2D and a Box Collider 2D - Made the Paddle of Player 1 to Move Up and Down when Holding the Up or Down Arrow Keys - Made the Paddle of Player 2 to Move Up and Down with the movement of the Mouse - Created a Script of the Paddle of Player 1 that allows the Movement of the Paddle with the Up and Down Arrow Keys - Created a Script of the Paddle of Player 2 that allows the Movement of the Paddle with the Movement of the Mouse - In both Scripts, I made both Paddles of the Players to be clamped to the height of the Screen

---
+++ Programming for Computer Games Assingment/Assets/Resources/Scripts/Player1Paddle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Paddle : MonoBehaviour {
+
+    float speed = 30f;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () { 
+   
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+
+            float KeyboardPaddle = (transform.position.y / Screen.height * 16) - 8;
+       
+            Vector3 KeyboardBorder = new Vector3(this.transform.position.x, KeyboardPaddle, this.transform.position.z);
+
+            KeyboardBorder.y = Mathf.Clamp(transform.position.y, -4.5548f, 4.5548f);
+
+            transform.position = KeyboardBorder;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+
+            float KeyboardPaddle = (transform.position.y / Screen.height * 16) - 8;
+           
+            Vector3 KeyboardBorder = new Vector3(this.transform.position.x, KeyboardPaddle, this.transform.position.z);
+
+            KeyboardBorder.y = Mathf.Clamp(transform.position.y, -4.5548f, 4.5548f);
+
+            transform.position = KeyboardBorder;
+        }
+
+
+    }
+}
---
+++ Programming for Computer Games Assingment/Assets/Resources/Scripts/Player2Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float MousePosUnits = (Input.mousePosition.y / Screen.height * 16) - 8;
+
+        Vector3 NewPaddlePos = new Vector3(this.transform.position.x, MousePosUnits, this.transform.position.z);
+
+        NewPaddlePos.y = Mathf.Clamp(MousePosUnits, -4.5548f, 4.5548f);
+
+        this.transform.position = NewPaddlePos;
+
+    }
+}
