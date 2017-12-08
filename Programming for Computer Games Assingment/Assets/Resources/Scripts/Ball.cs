50f4da8d7ade9461e6b9048ab07bfa5b15e912b7
RaulM10 <raul.mifsud@hotmail.com>
2017-12-08 23:33:13 +0100

    - Made the Ball to have a RigidBody 2D and a Circle Collider 2D - Added a Material called Bouncy Material - Added the newly created Material to the Ball so the Ball can bounce if it has been hit - Added a Script for the Ball and linked the Script to the Ball - In the Script, first I set the body type of the Ball to Static - Then, In the Script, I made the ball to begin Bouncing when one of the Players hit the Spacebar

---
+++ Programming for Computer Games Assingment/Assets/Resources/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    bool GameStarted = false;
+    private int numberOfThrows = 0;
+
+    // Use this for initialization
+    void Start () {
+        this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (numberOfThrows % 2 == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !GameStarted)
+            {
+                GameStarted = true;
+                this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+                numberOfThrows++;
+                print(numberOfThrows);
+            }
+        }
+
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !GameStarted)
+            {
+                GameStarted = true;
+                this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(-2f, 10f);
+                numberOfThrows++;
+                print(numberOfThrows);
+            }
+        }
+      
+
+    }
+}
